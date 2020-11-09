294774e0e1a8795761f3697d6307fc2827b82cce
SanghooMoon <sanghoo.moon@gmail.com>
2020-11-09 16:32:54 +0900

    사용자 목록 조회를 위한 API 구현 - GET HTTP Method

---
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -0,0 +1,29 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private UserDaoService service;
+
+    // 생성자를 통한 의존성 주입
+    public UserController(UserDaoService service){
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    // GET, /users/1 or /users/10 -> 서버측에 전달할때 String이 기본
+    @GetMapping("/users/{id}")          // int로 선언하면 자동 형변환(String -> int)
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+}

a2c851ba05cf6dede6ec6bbe381e8bc5606d533b
SanghooMoon <sanghoo.moon@gmail.com>
2020-11-10 18:32:34 +0900

    Swagger Documentation 구현 방법(커스터마이징)

--- src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -6,6 +6,8 @@ import org.springframework.hateoas.client.LinkDiscoverer;
 import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,6 +18,16 @@ import java.util.*;
 @EnableSwagger2 // Swagger 임
 public class SwaggerConfig {
 
+    private static final Contact DEFAULT_CONTATCT = new Contact("Sanghoo Moon", "http:/www.joneconsulting.co.kr",
+            "sanghoo96@naver.com");
+
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title", "My User management REST API Service",
+            "1.0", "urn:tos", DEFAULT_CONTATCT, "Apache 2.0",
+            "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/json", "application/xml"));
+
+
     @Bean
     public LinkDiscoverers discoverers() {
         List<LinkDiscoverer> plugins = new ArrayList<>();
@@ -25,6 +37,9 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -3,6 +3,8 @@ package com.example.restfulwebservice.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,16 +18,22 @@ import java.util.Date;
 @NoArgsConstructor
 //@JsonIgnoreProperties(value = {"password", "ssn"})      // 일괄처리하여 데이터를 클라이언트에게 노출시키지 않는 어노테이션
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체") // API 문서에 설명을 추가
 public class User {
 
     private Integer id;
 
     @Size(min=2, message = "Name은 2글자 이상 입력해주세요.")    // 2글자 이상 사용해야함.
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
     @Past           // 현재보다 과거의 데이터만 사용할 수 있다는 제약조건
+    @ApiModelProperty(notes = "사용자 등록일을 입력해 주세요.")
     private Date joinDate;
 
 //    @JsonIgnore     // 하나의 데이터(필드)를 클라이언트에 노출 시키지않기 위한 어노테이션
+
+    @ApiModelProperty(notes = "사용자 패스워드를 입력해 주세요.")
     private String password;
+    @ApiModelProperty(notes = "사용자 주민번호를 입력해 주세요.")
     private String ssn;
 }

1673d2126abbd92ea3c61a308295008c2f2403f3
SanghooMoon <sanghoo.moon@gmail.com>
2020-11-11 14:58:48 +0900

    Spring Service 구현을 위한 Controller, Repository 생성

---
+++ src/main/java/com/example/restfulwebservice/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // DB 관련된 Bean 의미                    //       PK 타입
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}

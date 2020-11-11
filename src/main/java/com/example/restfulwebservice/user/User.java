a7f0aedf98dbec710ca02746b15663a6920cfee0
SanghooMoon <sanghoo.moon@gmail.com>
2020-11-11 14:16:40 +0900

    Spring Data JPA를 이용한 Entity 설정과 초기 데이터 생성

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -9,6 +9,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -19,8 +22,11 @@ import java.util.Date;
 //@JsonIgnoreProperties(value = {"password", "ssn"})      // 일괄처리하여 데이터를 클라이언트에게 노출시키지 않는 어노테이션
 //@JsonFilter("UserInfo")
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체") // API 문서에 설명을 추가
+@Entity     // 해당하는 클래스를 가지고 DB에 테이블을 만듬
 public class User {
 
+    @Id                 // PK 의미
+    @GeneratedValue     // PK 값을 위한 자동 생성 전략을 명시하는데 사용
     private Integer id;
 
     @Size(min=2, message = "Name은 2글자 이상 입력해주세요.")    // 2글자 이상 사용해야함.
